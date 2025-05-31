75f13cfa4bd8c1532c8b0930c8716f0233e14b6b
koundinya@k <koundka@amazon.com>
2025-05-31 11:57:37 +0530

    Update App.js

--- 03-blog-app/src/App.js
+++ 03-blog-app/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Welcome to the Blog App</h1>
+        <h1>Welcome to our Blog App</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
